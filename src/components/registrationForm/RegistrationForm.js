61a8785af3667e35f2cdbc9d66b32a9ff4e9ce49
mjrgeorge <mjrgeorge@gmail.com>
2020-10-03 18:34:23 +0600

    VolunteerList component added

--- src/components/registrationForm/RegistrationForm.js
+++ src/components/registrationForm/RegistrationForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 
 const RegistrationForm = () => {
--- src/components/volunteerList/VolunteerList.js
+++ src/components/volunteerList/VolunteerList.js
@@ -1,9 +1,122 @@
 import React from 'react';
+import { Button, Table } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.png';
+import user from '../../images/users.png';
+import plus from '../../images/plus.png';
+import trash from '../../images/trash.png';
 
 const VolunteerList = () => {
     return (
         <div className="container bg-light">
-            <h1>This is volunteer list component</h1>
+            <div className="row">
+                <div className="col-md-3">
+                    <div className="m-4">
+                        <img style={{height: '70px'}} src={logo} alt="log"/>
+                    </div>
+                    <div className="m-4">
+                        <Link>
+                            <img style={{height: '20px'}} src={user} alt="User"/>
+                            Volunteer register list
+                        </Link>
+                    </div>
+                    <Button className="ml-4" variant="light">
+                        <img style={{height: '20px'}} src={plus} alt="Plus"/>
+                        Add Event
+                    </Button>
+                </div>
+                <div className="col-md-9">
+                    <h3 className="mt-5">Volunteer register list</h3>
+                    <Table style={{border: '15px solid white', borderRadius:'10px'}} striped bordered hover>
+                        <thead className="text-center">
+                            <tr>
+                                <th>Name</th>
+                                <th>First Email Id</th>
+                                <th>Registration Date</th>
+                                <th>Volunteer</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Jubayer Rahman</td>
+                                <td>mjrgeorge@gmail.com</td>
+                                <td>22-10-2020</td>
+                                <td>Organize books at the library</td>
+                                <td className="text-center">
+                                    <img className="rounded bg-danger p-1" style={{height: '30px', cursor: 'pointer'}} src={trash} alt="User"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td>Jubayer Rahman</td>
+                                <td>mjrgeorge@gmail.com</td>
+                                <td>22-10-2020</td>
+                                <td>Organize books at the library</td>
+                                <td className="text-center">
+                                    <img className="rounded bg-danger p-1" style={{height: '30px', cursor: 'pointer'}} src={trash} alt="User"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td>Jubayer Rahman</td>
+                                <td>mjrgeorge@gmail.com</td>
+                                <td>22-10-2020</td>
+                                <td>Organize books at the library</td>
+                                <td className="text-center">
+                                    <img className="rounded bg-danger p-1" style={{height: '30px', cursor: 'pointer'}} src={trash} alt="User"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td>Jubayer Rahman</td>
+                                <td>mjrgeorge@gmail.com</td>
+                                <td>22-10-2020</td>
+                                <td>Organize books at the library</td>
+                                <td className="text-center">
+                                    <img className="rounded bg-danger p-1" style={{height: '30px', cursor: 'pointer'}} src={trash} alt="User"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td>Jubayer Rahman</td>
+                                <td>mjrgeorge@gmail.com</td>
+                                <td>22-10-2020</td>
+                                <td>Organize books at the library</td>
+                                <td className="text-center">
+                                    <img className="rounded bg-danger p-1" style={{height: '30px', cursor: 'pointer'}} src={trash} alt="User"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td>Jubayer Rahman</td>
+                                <td>mjrgeorge@gmail.com</td>
+                                <td>22-10-2020</td>
+                                <td>Organize books at the library</td>
+                                <td className="text-center">
+                                    <img className="rounded bg-danger p-1" style={{height: '30px', cursor: 'pointer'}} src={trash} alt="User"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td>Jubayer Rahman</td>
+                                <td>mjrgeorge@gmail.com</td>
+                                <td>22-10-2020</td>
+                                <td>Organize books at the library</td>
+                                <td className="text-center">
+                                    <img className="rounded bg-danger p-1" style={{height: '30px', cursor: 'pointer'}} src={trash} alt="User"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+            </div>
         </div>
     );
 };
