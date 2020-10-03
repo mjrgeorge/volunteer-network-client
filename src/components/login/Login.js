7c5f42bd371f10bddea3d9fc13606e5efcdca214
mjrgeorge <mjrgeorge@gmail.com>
2020-10-03 22:02:11 +0600

    firebase aut added

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        photo: ""
+    });
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const handleSignIn =()=>{
+        firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const {displayName, email, photoURL} = result.user;
+            const signedInUser = {
+                name: displayName,
+                email: email,
+                photo: photoURL
+            }
+            setUser(signedInUser)
+          })
+          .catch(error => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+          });
+    };
+
     return (
         <div className="container bg-light pt-5">
             <div style={{ height:'80px'}} className="d-flex justify-content-center">
@@ -12,7 +41,7 @@ const Login = () => {
             <div className="d-flex justify-content-center align-items-center">
                 <div className="m-5 p-5 text-center" style={{width: '50%', border: '5px solid lightGray', borderRadius: '10px'}}>
                     <h2>Login With</h2>
-                    <Button className="rounded-pill p-3 m-4" block>
+                    <Button onClick={handleSignIn} className="rounded-pill p-3 m-4" block>
                         Continue with Google
                     </Button>
                     <p>Don't have an account? <Link>Create an account</Link></p>
---
+++ src/components/login/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBG6ae2Yu0cwlTm8sco9WMEN0FgXQEXj5w",
+    authDomain: "volunteer-network-22cfc.firebaseapp.com",
+    databaseURL: "https://volunteer-network-22cfc.firebaseio.com",
+    projectId: "volunteer-network-22cfc",
+    storageBucket: "volunteer-network-22cfc.appspot.com",
+    messagingSenderId: "270329663660",
+    appId: "1:270329663660:web:9b107b4037f2ff89f90f09"
+  };
+
+  export default firebaseConfig;
