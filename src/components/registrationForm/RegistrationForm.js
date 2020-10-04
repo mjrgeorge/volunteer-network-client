451256c40951da00d887b6aede000dc984908cc0
mjrgeorge <mjrgeorge@gmail.com>
2020-10-04 11:30:28 +0600

    somthing update

--- src/components/registrationForm/RegistrationForm.js
+++ src/components/registrationForm/RegistrationForm.js
@@ -1,12 +1,31 @@
 import React, { useContext } from 'react';
 import { Button } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { UserContext } from '../../App';
 import logo from '../../images/logo.png';
 
 const RegistrationForm = () => {
     const {jobTitle} = useParams();
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    const data = {name: 'Asmaul husna', job: 'doctor'};
+    const history = useHistory();
+    const handleSubmit =()=> {
+        fetch('http://localhost:5000/addUser', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(result =>{
+            if(result){
+                history.push(`/activities`);
+            }
+        })
+        .catch(err => console.log(err));
+
+    };
+
     return (
         <div className="container bg-light p-3">
             <Link to="/home">
@@ -17,7 +36,7 @@ const RegistrationForm = () => {
             <div className="d-flex justify-content-center align-items-center m-4">
                 <div className="p-5 text-center" style={{width: '50%', border: '3px solid lightGray', borderRadius: '10px'}}>
                     <h3 className="mb-3">Registration as a Volunteer</h3>
-                    <form action="">
+                    <form>
                         <input className="form-control" type="text" name="name" defaultValue={`${loggedInUser.name}`} placeholder="Full Name" required/>
                         <br/>
                         <input className="form-control" type="email" name="email" defaultValue={`${loggedInUser.email}`} placeholder="Email" required/>
@@ -28,10 +47,8 @@ const RegistrationForm = () => {
                         <br/>
                         <input className="form-control" type="text" name="jobs" defaultValue={`${jobTitle}`} placeholder="Organization books at the library" required/>
                         <br/>
+                        <Button onClick={handleSubmit} block>Registration</Button>
                     </form>
-                    <Link to="/activities" className="text-decoration-none">
-                        <Button type="submit" block>Registration</Button>
-                    </Link>
                 </div>
             </div>
         </div>
